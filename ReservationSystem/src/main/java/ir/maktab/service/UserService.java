322b44619318d79825bf8b1e18d94856b945ba91
MORTEZA <morteza_ahangari_1994@yahoo.com>
2021-10-02 04:57:27 +0330

    Add UserService and UserServiceImpl

---
+++ ReservationSystem/src/main/java/ir/maktab/service/UserService.java
@@ -0,0 +1,6 @@
+package ir.maktab.service;
+
+import ir.maktab.domain.User;
+
+public interface UserService extends BaseUserService<User, String, String> {
+}
---
+++ ReservationSystem/src/main/java/ir/maktab/service/impl/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package ir.maktab.service.impl;
+
+import ir.maktab.domain.User;
+import ir.maktab.repository.BaseUserRepository;
+import ir.maktab.service.UserService;
+
+public class UserServiceImpl extends BaseUserServiceImpl<User, String, String> implements UserService {
+
+    public UserServiceImpl(BaseUserRepository<User, String, String> repository) {
+        super(repository);
+    }
+
+}
