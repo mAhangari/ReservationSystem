a1ffa8d07aabbde6bee9c645720437376fda91c4
MORTEZA <morteza_ahangari_1994@yahoo.com>
2021-10-04 02:02:14 +0330

    Modify UserFilter set Attribute for request

--- ReservationSystem/src/main/java/ir/maktab/controller/filter/UserFilter.java
+++ ReservationSystem/src/main/java/ir/maktab/controller/filter/UserFilter.java
@@ -5,7 +5,6 @@ import ir.maktab.service.ExceptionHandling.AccountNotFoundException;
 import ir.maktab.util.ApplicationContext;
 import javax.servlet.*;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class UserFilter implements Filter {
     @Override
@@ -15,16 +14,14 @@ public class UserFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
         response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         try {
             User account = ApplicationContext.loginServ.login(username, password);
+            request.setAttribute("account", account);
             filterChain.doFilter(request, response);
         } catch (AccountNotFoundException a) {
-            out.println("<h1>Sorry username or password incorrect!!!</h1>");
-            RequestDispatcher requestDispatcher = request.getRequestDispatcher("/index.jsp");
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("/src/main/views/login_failed.jsp");
             requestDispatcher.include(request, response);
         }
     }
