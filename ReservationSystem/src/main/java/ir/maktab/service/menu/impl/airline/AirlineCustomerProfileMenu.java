adf725f1a524de693156e32d26b1217a76891883
MORTEZA <morteza_ahangari_1994@yahoo.com>
2021-10-06 18:23:30 +0330

    Modify AirlineCustomerProfileMenu remove Scanner

--- ReservationSystem/src/main/java/ir/maktab/service/menu/impl/airline/AirlineCustomerProfileMenu.java
+++ ReservationSystem/src/main/java/ir/maktab/service/menu/impl/airline/AirlineCustomerProfileMenu.java
@@ -4,11 +4,8 @@ import ir.maktab.model.Customer;
 import ir.maktab.service.menu.ProfileMenu;
 import lombok.AccessLevel;
 import lombok.Getter;
-import java.util.*;
 
 public class AirlineCustomerProfileMenu implements ProfileMenu {
-
-    Scanner input = new Scanner(System.in);
 
     @Getter(AccessLevel.PRIVATE)
     private final Customer customer;
