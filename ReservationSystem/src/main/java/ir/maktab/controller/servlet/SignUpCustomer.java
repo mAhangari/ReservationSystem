f366c10c823f4c393cea8d13b2baaa117b2be61d
MORTEZA <morteza_ahangari_1994@yahoo.com>
2021-10-06 20:49:51 +0330

    Add SignUpCustomer

---
+++ ReservationSystem/src/main/java/ir/maktab/controller/servlet/SignUpCustomer.java
@@ -0,0 +1,56 @@
+package ir.maktab.controller.servlet;
+
+import ir.maktab.model.Customer;
+import ir.maktab.util.ApplicationContext;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Objects;
+
+public class SignUpCustomer extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        RequestDispatcher requestDispatcher;
+
+        if (!Objects.equals(request.getParameter("password"), request.getParameter("passRepeat"))) {
+            request.setAttribute("message", "Passwords does not match!!!");
+            requestDispatcher = request.getRequestDispatcher("/src/main/views/profile/customer/sign_up.jsp");
+            requestDispatcher.forward(request, response);
+        } else {
+
+            boolean check = ApplicationContext.userServ.existsByUsername(request.getParameter("username"));
+            if (check) {
+                request.setAttribute("message", "This Username is already Exists.");
+                requestDispatcher = request.getRequestDispatcher("/src/main/views/profile/customer/sign_up.jsp");
+            } else {
+
+                Customer customer = new Customer(request.getParameter("username"),
+                        request.getParameter("password"),
+                        request.getParameter("firstName"),
+                        request.getParameter("lastName"),
+                        request.getParameter("nationalCode"));
+
+                ApplicationContext.userServ.save(customer);
+                request.setAttribute("message", "Congratulations!" +
+                        request.getParameter("firstName") +
+                        " " + request.getParameter("lastName") +
+                        "Your Registration was Successful!!!");
+
+                requestDispatcher = request.getRequestDispatcher("/index.jsp");
+            }
+            requestDispatcher.forward(request, response);
+        }
+    }
+}
