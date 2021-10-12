ed2a86b863d121703114b123274008b47d6f0f5b
MORTEZA <morteza_ahangari_1994@yahoo.com>
2021-10-12 07:13:23 +0330

    Modify BlitController add wallet changes

--- ReservationSystem/src/main/java/ir/maktab/controller/servlet/BlitController.java
+++ ReservationSystem/src/main/java/ir/maktab/controller/servlet/BlitController.java
@@ -3,6 +3,7 @@ package ir.maktab.controller.servlet;
 import ir.maktab.model.Customer;
 import ir.maktab.model.FlightSchedule;
 import ir.maktab.model.Ticket;
+import ir.maktab.model.Wallet;
 import ir.maktab.util.ApplicationContext;
 
 import javax.servlet.RequestDispatcher;
@@ -17,29 +18,37 @@ public class BlitController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         RequestDispatcher requestDispatcher;
         HttpSession session = request.getSession(false);
 
-        if (request.getParameter("flightOption") != null) {
 
-            Long flightId = Long.valueOf(request.getParameter("flightOption"));
-            FlightSchedule flightSchedule = ApplicationContext.flightScheduleServ.findById(flightId);
+        Long flightId = Long.valueOf((String) session.getAttribute("flightOpt"));
+        FlightSchedule flightSchedule = ApplicationContext.flightScheduleServ.findById(flightId);
 
-            Customer customer = (Customer) session.getAttribute("account");
+        Customer customer = (Customer) session.getAttribute("account");
+
+        Wallet wallet = ApplicationContext.walletServ.findById(customer.getWallet().getId());
+        if (wallet.getTotalAmount() >= Double.parseDouble((String) session.getAttribute("totalAmount"))) {
+            wallet.setCashAmount(customer.getWallet().getCashAmount() - Double.parseDouble((String) session.getAttribute("totalAmount")));
+            ApplicationContext.walletServ.save(wallet);
+
             Ticket customerTicket = new Ticket();
             customerTicket.setCustomer(customer);
             customerTicket.setFlightSchedule(flightSchedule);
-
             ApplicationContext.ticketServ.save(customerTicket);
 
             request.setAttribute("message", "Congratulations, the ticket purchase was successful !!!");
 
-            requestDispatcher = request.getRequestDispatcher("/src/main/views/profile/customer/buy_ticket.jsp");
         } else {
-            request.setAttribute("message", "No tickets selected!!!");
-            requestDispatcher = request.getRequestDispatcher("/src/main/views/profile/customer/show_flight_schedule.jsp");
+            request.setAttribute("message", "Account balance is not enough !!!");
         }
+        requestDispatcher = request.getRequestDispatcher("/src/main/views/profile/customer/buy_ticket.jsp");
         requestDispatcher.forward(request, response);
     }
 }
